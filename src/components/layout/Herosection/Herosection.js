010d1cb4aa2178cf9bb297b8dc127163e17fd097
Samy Zogeyb <samyzogeyb@gmail.com>
2021-09-06 18:24:15 +0400

    Update Herosection.js

--- src/components/layout/Herosection/Herosection.js
+++ src/components/layout/Herosection/Herosection.js
@@ -24,7 +24,7 @@ const Herosection = (props) => {
 					<Basebutton as="a" href="#contact">
 						CONTACT ME <RiChat4Line />
 					</Basebutton>
-					<Basebutton as="a" target="_blank" href="/Samy_Zogeyb_Resume.pdf">
+					<Basebutton as="a" target="_blank" href="/Samy_Zogeyb_Resume.pdf" download="Samy Zogeyb Resume">
 						DOWNLOAD CV <RiProfileLine />
 					</Basebutton>
 				</Hstack>
